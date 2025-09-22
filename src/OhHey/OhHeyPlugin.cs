1e844f77bff8e64357f3b2f705627e1b258678e4
MeiHasCrashed <meihascrashed@outlook.com>
2025-09-22 15:43:37 +0200

    Initial plugin setup.

---
+++ src/OhHey/OhHeyPlugin.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2025 MeiHasCrashed
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace OhHey;
+
+public class OhHeyPlugin : IDalamudPlugin
+{
+    public OhHeyPlugin(IPluginLog logger)
+    {
+        logger.Info("OhHey loading started.");
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
