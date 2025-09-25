b88a270e9832e6b60d41d7225830437c27ca7b9d
MeiHasCrashed <meihascrashed@outlook.com>
2025-09-25 18:04:46 +0200

    Added help command and command to add settings menu

--- src/OhHey/Services/ChatCommandService.cs
+++ src/OhHey/Services/ChatCommandService.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Dalamud.Game.Command;
+using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Plugin.Services;
 using OhHey.UI;
 
@@ -11,22 +12,59 @@ public sealed class ChatCommandService : IDisposable
 {
     private const string CommandName = "/ohhey";
     private readonly ICommandManager _commandManager;
+    private readonly IChatGui _chatGui;
+    private readonly IPluginLog _logger;
     private readonly MainWindow _mainWindow;
+    private readonly ConfigurationWindow _configWindow;
 
-    public ChatCommandService(ICommandManager commandManager, MainWindow mainWindow)
+    public ChatCommandService(ICommandManager commandManager, IChatGui chatGui, IPluginLog logger, MainWindow mainWindow, ConfigurationWindow configWindow)
     {
         _commandManager = commandManager;
+        _chatGui = chatGui;
+        _logger = logger;
         _mainWindow = mainWindow;
+        _configWindow = configWindow;
 
         _commandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
-            HelpMessage = "Opens the OhHey main window."
+            HelpMessage = "Opens the OhHey main window if used by itself or when using '/ohhey main'.\n" +
+                          "Use '/ohhey config' to open the configuration window.",
         });
     }
 
-    private void OnCommand(string command, string args)
+    private void OnCommand(string command, string argsString)
     {
-        _mainWindow.Toggle();
+        var args = argsString.Split(" ").Select(x => x.Trim()).ToArray();
+        _logger.Debug("CommandInput: {Command} Args: {Args} Raw Args: {RawArgs}", command, args, argsString);
+        if (args.Length == 0 || string.IsNullOrWhiteSpace(argsString))
+        {
+            _mainWindow.Toggle();
+            return;
+        }
+        switch (args[0].ToLower())
+        {
+            case "main":
+                _mainWindow.Toggle();
+                break;
+            case "config":
+                _configWindow.Toggle();
+                break;
+            default:
+                ShowHelp();
+                break;
+        }
+    }
+
+    private void ShowHelp()
+    {
+        var builder = new SeStringBuilder();
+        builder
+            .AddUiForeground("[Oh Hey!] Chat Commands: \n\n", 537).AddUiForegroundOff()
+            .AddText("- ").AddUiForeground("/ohhey", 37).AddUiForegroundOff().AddText(" Opens the main window.\n")
+            .AddText("- ").AddUiForeground("/ohhey main", 37).AddUiForegroundOff().AddText(" Opens the main window.\n")
+            .AddText("- ").AddUiForeground("/ohhey config", 37).AddUiForegroundOff().AddText(" Opens the configuration window.\n")
+            .AddText("- ").AddUiForeground("/ohhey help", 37).AddUiForegroundOff().AddText(" Shows this help message.");
+        _chatGui.Print(builder.Build());
     }
 
 
